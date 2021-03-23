1b4d0f3aef210b9b4efa1be17adb051435cae8f6
John-Allan Lambert <asowner@outlook.com>
2021-03-23 23:26:05 +0000

    eslint

--- commands/fun/cat.js
+++ commands/fun/cat.js
@@ -1,3 +1,4 @@
+/* eslint-disable comma-dangle */
 /* eslint-disable no-unused-vars */
 const Discord = require('discord.js');
 const { Client, Message } = require('discord.js');
@@ -9,14 +10,16 @@ const fetch = require('node-fetch').default;
  * @param {String[]} args
  */
 module.exports.run = async (client, message, args) => {
-	// Snowflake API for the cat images 
-	fetch("https://api.snowflakedev.xyz/api/cat", {
-        headers: {
-            "Authorization": "YOUR_API_KEY" // Change this API key if you have premium
-    }})
+	// Snowflake API for the cat images
+	fetch('https://api.snowflakedev.xyz/api/cat', {
+		headers: {
+			'Authorization': 'YOUR_API_KEY' // Change this API key if you have premium
+		}
+	})
 		.then(response => response.json())
 		.then(data => {
-			message.reply(); // TODO: Reply with image (API returns image binary)
+			console.log(data); // Debug
+			message.reply(data); // TODO: Reply with image (API returns image binary)
 		});
 };
 
