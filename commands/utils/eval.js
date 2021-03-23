3eae822fc3a01ebab42485093cff0fe78a29725c
birb <62964708+birbified@users.noreply.github.com>
2021-03-23 17:58:13 -0400

    Create eval.js

---
+++ commands/utils/eval.js
@@ -0,0 +1,24 @@
+module.exports.run = async (client, message, args) => {
+	args = args.join(' ');
+    try {
+        let evaled = eval(args);
+        if (typeof evaled !== 'string') {
+            evaled = require('util').inspect(evaled);
+        }
+        message.channel.send(`\`\`\`xl\n${clean(evaled)}\n\`\`\``);
+    }
+    catch (err) {
+        message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
+    }
+};
+module.exports.config = {
+	name: 'eval',
+	aliases: [],
+};
+
+function clean(text) {
+	if (typeof (text) === 'string') {
+        return text.replace(/`/g, '`' + String.fromCharCode(8203)).replace(/@/g, '@' + String.fromCharCode(8203));
+    }
+	else return text;
+}
