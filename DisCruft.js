15670dba679076a5d2e0aee643f7d972d5887776
Assassin-1234 <chordsandfrets@gmail.com>
2021-03-23 11:52:46 +0530

    Update DisCruft.js

--- DisCruft.js
+++ DisCruft.js
@@ -13,6 +13,6 @@ module.exports = class DisCruft extends Client {
 	setup() {
 		require('./utils/loadCommands')(this);
 		require('./utils/loadEvents')(this);
-		this.login('NzQxMDAwODY1Mjg4MjkwNDM1.XyxM1Q.FrGWuUDFU8jF6qWo8tH5hwJkLCA');
+		this.login(process.env.TOKEN);
 	}
 };
