f404a6e57064cbc03ded04de63f55b0b36ec92e2
birb <62964708+birbified@users.noreply.github.com>
2021-03-23 13:01:02 -0400

    Update DatabaseManager.js

--- database/DatabaseManager.js
+++ database/DatabaseManager.js
@@ -24,7 +24,41 @@ module.exports = class DatabaseManagerr {
         const data = await this.model.findOne(query);
         if (!data) return;
         
-        this.cache.set(data._id.toString());
+        this.cache.set(data._id.toString(), data);
         return data;
+    }
+    
+    async find(query) {
+        const data = await this.model.find(query);
+        if (data.length < 1) return;
+        
+        for (const e of data) {
+            if (!this.cache.has(e._id.toString())) this.cache.set(e._id.toString(), e);
+        }
+        
+        return data;
+    }
+    
+    async updateOne(query, doc) {
+        const data = await this.findOne(query);
+        if (!data) return;
+        
+        await this.model.updateOne(query, doc);
+        const e = this.cache.set(data._id.toString(), doc);
+    }
+    
+    async updateMany(query, doc) {
+        const data = await this.find(query);
+        if (!data) return;
+        
+        await this.model.updateMany(query, doc);
+        for (const e of data) {
+            this.cache.set(e._id.toString(), e);
+        }
+    }
+    
+    async insertOne(doc) {
+        const data = await this.model(doc).save();
+        this.cache.set(data._id.toString(), data);
     }
 }
