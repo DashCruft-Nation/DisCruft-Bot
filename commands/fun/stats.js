013ed8db2b58af028557b28eb82db289799af3d2
Assassin#1234 <chordsandfrets@gmail.com>
2021-03-23 15:12:51 +0530

    bruh moment

--- commands/fun/stats.js
+++ commands/fun/stats.js
@@ -4,7 +4,7 @@ module.exports.run = async (client, message, args) => {
     const msg = await message.reply('Loading...').then(m => m.delete(), 1000);
     const embed = new Discord.MessageEmbed()
         .setAuthor('DisCruft bot stats')
-        .addField('__Cache__\n', `**Guilds**: ${bot.guilds.cache.size} \n\n`, true)
+        .addField('__Cache__\n', `**Guilds**: ${client.guilds.cache.size} \n\n`, true)
         .addField('__Uptime__\n', `**Process**: ${prettyMs(process.uptime() * 1000)}`, true)
         .addField('__Movement__\n', `**API Latency**: ${Math.round(bot.ws.ping)}\n**Bot Latency**: ${msg.createdTimestamp - message.createdTimestamp}ms`, true);
     message.reply(embed);
