ed38d629b19f1b9611fce912464ef887e1504560
LegIt <77503777+LegItMate@users.noreply.github.com>
2021-03-23 15:08:49 +0530

    Bruh dont copy next time

--- commands/fun/stats.js
+++ commands/fun/stats.js
@@ -1,15 +1,15 @@
-module.exports.run = async (client, message, args) => {
 const Discord = require('discord.js');
-const prettyMs = require('pretty-ms')
-const msg = await message.reply(`Loading...`).then(m  => m.delete(), 1000)
-let embed = new Discord.MessageEmbed()
-.setAuthor(`Weky bot stats`)
-.addField(`__Cache__\n`,`**Guilds**: ${bot.guilds.cache.size} \n\n`, true)
-.addField(`__Uptime__\n`,`**Process**: ${prettyMs(process.uptime() * 1000)}`, true)
-.addField(`__Movement__\n`,`**API Latency**: ${Math.round(bot.ws.ping)}\n**Bot Latency**: ${msg.createdTimestamp - message.createdTimestamp}ms`, true)
-message.reply(embed)
+const prettyMs = require('pretty-ms');
+module.exports.run = async (client, message, args) => {
+    const msg = await message.reply('Loading...').then(m => m.delete(), 1000);
+    const embed = new Discord.MessageEmbed()
+        .setAuthor('DisCruft bot stats')
+        .addField('__Cache__\n', `**Guilds**: ${bot.guilds.cache.size} \n\n`, true)
+        .addField('__Uptime__\n', `**Process**: ${prettyMs(process.uptime() * 1000)}`, true)
+        .addField('__Movement__\n', `**API Latency**: ${Math.round(bot.ws.ping)}\n**Bot Latency**: ${msg.createdTimestamp - message.createdTimestamp}ms`, true);
+    message.reply(embed);
 };
 module.exports.config = {
-	name: 'stats',
-	aliases: [],
+    name: 'stats',
+    aliases: [],
 };
