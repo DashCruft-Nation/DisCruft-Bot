48d92906cd9c7ab2908fa62fef2d55408e86aa14
ImFace <78975742+ImFace@users.noreply.github.com>
2021-03-23 10:58:43 +0200

    Create lock.js
    NO

---
+++ lock.js
@@ -0,0 +1,20 @@
+module.exports.run = async (client, message, args) => {
+if (!message.member.permissions.has("MANAGE_CHANNELS")) return message.channel.send('You don\'t have permission (MANAGE_CHANNELS) to use this command').then(m => m.delete({timeout: 5000}));
+    if (!message.guild.me.hasPermission(['MANAGE_CHANNELS'])) return message.channel.send('I don\'t have permission to use that command').then(m => m.delete({timeout: 5000}))
+   message.channel.overwritePermissions([
+     {
+        id: message.guild.id,
+        deny : ['SEND_MESSAGES'],
+     },
+    ],);
+   const embed = new Discord.MessageEmbed()
+   .setTitle("Channel Updates")
+   .setDescription(`${message.channel} has been Locked`)
+   .setColor("RANDOM");
+   await message.channel.send(embed);
+   message.delete();
+};
+module.exports.config = {
+	name: 'lock',
+	aliases: [],
+};
