e02c4866ef573475b1b4cd68c4cbfc0c349714f2
DashCruft <theporyafm12@gmail.com>
2021-03-23 15:56:44 -0700

    gay command has a bigger picture now

--- commands/fun/gay.js
+++ commands/fun/gay.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 const fetch = require('node-fetch').default;
 module.exports.run = async (client, message, args) => {
 	const member = message.mentions.members.first() || message.member;
-    const buffer = await (await fetch('https://api.monkedev.com/canvas/gay?imgUrl=' + encodeURIComponent(member.user.displayAvatarURL({ format: 'jpg' })))).buffer();
+    const buffer = await (await fetch('https://api.monkedev.com/canvas/gay?imgUrl=' + encodeURIComponent(member.user.displayAvatarURL({ size: 1024, format: 'jpg' })))).buffer();
 	const attachment = new Discord.MessageAttachment(buffer, `gay.jpg`);
 	message.reply({ files: [attachment], allowedMentions: { repliedUser: false } });
 };
