cc5504bb73583b85b864731efd0d1f9c10ec011e
Porya <theporyafm12@gmail.com>
2021-03-22 22:21:29 -0700

    DJS docs cmd + node-fetch npm package

---
+++ commands/docs.js
@@ -0,0 +1,24 @@
+const fetch = require("node-fetch").default;
+
+module.exports.run = async (client, message, args) => {
+    let [query, branch] = args;
+
+    if (!query) return message.reply("No results!");
+    if (!branch) branch = "master";
+
+    fetch(`https://djsdocs.sorta.moe/v2/embed?src=${branch}&q=${encodeURIComponent(query)}`)
+        .then(res => res.json())
+        .then(json => {
+            if (!json) return message.channel.send("Not found!");
+
+            message.channel.send({ embed: json });
+        })
+        .catch(() => {
+            message.channel.send("Couldn't fetch docs!");
+        })
+}
+
+module.exports.config = {
+    name: "docs",
+    aliases: ['djs']
+}
