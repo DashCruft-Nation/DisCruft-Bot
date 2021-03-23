a9aa51df3a43935c725e886f85c63a62777bdb5b
Mr. Monkey <69441882+MrM0nkeyz@users.noreply.github.com>
2021-03-23 11:16:31 -0400

    Update avatar.js
    Members can be mentioned with id, username, mention, or by author instead of just mention or author.

--- commands/utils/avatar.js
+++ commands/utils/avatar.js
@@ -1,7 +1,11 @@
 /* eslint-disable no-unused-vars */
 const Discord = require('discord.js');
 module.exports.run = async (client, message, args) => {
-	const user = message.mentions.users.first() || message.author;
+
+    let userArray = message.content.split(" ");
+    let userArgs = userArray.slice(1);
+    let user = message.mentions.members.first() || message.guild.members.cache.get(userArgs[0]) || message.guild.members.cache.find(x => x.user.username.toLowerCase() === userArgs.slice(0).join(" ") || x.user.username === userArgs[0]) || message.member;
+
 	const embed = new Discord.MessageEmbed()
 		.setTitle(`${user.tag}'s avatar!`)
 		.setColor('#e4101f')
