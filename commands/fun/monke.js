8e43f92f3b4e5c41de21d359d11469fcf3f48af4
DashCruft <theporyafm12@gmail.com>
2021-03-23 16:05:06 -0700

    yes

--- commands/fun/monke.js
+++ commands/fun/monke.js
@@ -5,7 +5,11 @@ module.exports.run = async (bot, message, args) => {
     fetch(`https://api.monkedev.com/attachments/monkey?key=YiGNicedKYp9c15HbrKrUkMkG`)
         .then(response => response.json())
         .then(data => {
-            message.reply(data.url)
+            message.reply(data.url, {
+                allowedMentions: {
+                    repliedUser: false
+                }
+            });
         })
         .catch(() => {
             message.reply("Couldn't fetch image!");
