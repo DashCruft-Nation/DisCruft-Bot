bebef9d6691115cce2fb27b785a5a12dcf654ef0
birb <62964708+birbified@users.noreply.github.com>
2021-03-23 13:11:13 -0400

    Update db.js

--- database/db.js
+++ database/db.js
@@ -1,19 +1,33 @@
 require('dotenv').config();
-
 const mongoose = require('mongoose');
 const fs = require('fs');
+const Manager = require('./DatabaseManager');
 
-module.exports = client => {
-    mongoose.connect(process.env.DATABASE, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    });
+class Database {
+    constructor() {
+        this.models = new Map();
+        
+        mongoose.connect(process.env.DATABASE, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+
+        mongoose.connection.on('connect', () => {
+            console.log('Connected to database.');
+        });
+
+        mongoose.connection.on('disconnect', () => {
+            console.log('Lost connection to database.');
+        });
+
+        const models = fs.readdirSync('./database/models/');
+        for (const file of models) {
+            const e = require(`./models/${file}`);
+            this.models.set(file.split('.')[0], new Manager(e));
+        }
+    }
     
-    mongoose.connection.on('connect', () => {
-        console.log('Connected to database.');
-    });
-    
-    mongoose.connection.on('disconnect', () => {
-        console.log('Lost connection to database.');
-    });
+    getModel(model) {
+        return this.models.get(model);
+    }
 }
