5b5298aa0ac789062ad27da5641126071f32de22
Assassin#1234 <chordsandfrets@gmail.com>
2021-03-23 21:21:00 +0530

    Update avatar.js

--- commands/utils/avatar.js
+++ commands/utils/avatar.js
@@ -1,10 +1,7 @@
 /* eslint-disable no-unused-vars */
 const Discord = require('discord.js');
 module.exports.run = async (client, message, args) => {
-
-    let userArray = message.content.split(" ");
-    let userArgs = userArray.slice(1);
-    let user = message.mentions.members.first() || message.guild.members.cache.get(userArgs[0]) || message.guild.members.cache.find(x => x.user.username.toLowerCase() === userArgs.slice(0).join(" ") || x.user.username === userArgs[0]) || message.member;
+    let user = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(x => x.user.username.toLowerCase() === args.slice(0).join(" ") || x.user.username === args[0]) || message.member;
 
 	const embed = new Discord.MessageEmbed()
 		.setTitle(`${user.tag}'s avatar!`)
