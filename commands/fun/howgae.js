dea04109a59a8642f59e2be59023adfdb5e94702
birbsnest <81213065+birbsnest@users.noreply.github.com>
2021-03-24 10:37:01 +0600

    howgae.js
    yeah new fun cmd add it it will be fun ngl

---
+++ commands/fun/howgae.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-unused-vars */
+const { Client, Message, MessageEmbed } = require('discord.js');
+/**
+ * JSDOC
+ * @param {Client} client
+ * @param {Message} message
+ * @param {String[]} args
+ */
+module.exports.run = async (client, message, args) => {
+  let replies = ["50 percent :rainbow_flag: ", "20 percent :rainbow_flag: ", "1 percent :rainbow_flag: ", "5 percent :rainbow_flag: ", "10 percent :rainbow_flag: ", "100 percent if you know it or not  YOU ARE GAE :rainbow_flag: ", "90 percent :rainbow_flag: ", "35 percent :rainbow_flag: ", "80 peercent :rainbow_flag: ", "85 percent :rainbow_flag: ", "6 percent :rainbow_flag: ", "GREAT YOU ARE NOT GAE!!! THANK GOD", "99 PERCENT :rainbow_flag: ", "idk"] //you can continue it like this , "another reply", "one more reply"]
+    message.channel.send(replies[Math.floor(Math.random() * replies.length)])
+};
+
+module.exports.config = {
+	name: 'howgae',
+	aliases: ['check if ur gae'],
+	description: 'Check how gay you are! promise it never lies',
