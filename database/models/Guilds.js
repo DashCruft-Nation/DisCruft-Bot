1376b398b0834f381490a06d8d00ffc4072d270e
Assassin-1234 <chordsandfrets@gmail.com>
2021-03-24 10:24:01 +0530

    lint errs

--- database/models/Guilds.js
+++ database/models/Guilds.js
@@ -1,8 +1,8 @@
 const { Schema, model } = require('mongoose');
 
 const Guilds = new Schema({
-    id: { type: String, required: true },
-    prefix: { type: String, default: '?' }
+	id: { type: String, required: true },
+	prefix: { type: String, default: '?' },
 });
 
 module.exports = model('Guilds', Guilds);
