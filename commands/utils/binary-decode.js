dcd5b3deaf5f0233ffdaf12ab26c36e601aeec20
DeadLordOfficial <68822716+DeadLordWasTaken@users.noreply.github.com>
2021-03-24 01:13:52 -0700

    Added reply function to binary-decode

--- commands/utils/binary-decode.js
+++ commands/utils/binary-decode.js
@@ -9,10 +9,10 @@ module.exports.run = async (client, message, args) => {
 	const {
 		text,
 	} = await (await fetch(`https://some-random-api.ml/binary?decode=${encodeURI(args[0])}`)).json();
-
-	return message.channel.send(new MessageEmbed()
+	const embed = new MessageEmbed()
 		.setTitle('Here is your decoded binary!')
-		.setDescription(`**Output:**\n${text}`));
+		.setDescription(`**Output:**\n${text}`);
+	return message.reply({ embed: embed, allowedMentions: { repliedUser: false } });
 };
 
 module.exports.config = {
