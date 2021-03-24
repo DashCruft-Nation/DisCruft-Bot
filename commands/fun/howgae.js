06c7880880f5ab4bc285ade79c4737dcec179a20
LegIt <77503777+LegItMate@users.noreply.github.com>
2021-03-24 12:21:09 +0530

    Me when no lint error fix

--- commands/fun/howgae.js
+++ commands/fun/howgae.js
@@ -7,8 +7,8 @@ const { Client, Message, MessageEmbed } = require('discord.js');
  * @param {String[]} args
  */
 module.exports.run = async (client, message, args) => {
-  let replies = ["50 percent :rainbow_flag: ", "20 percent :rainbow_flag: ", "1 percent :rainbow_flag: ", "5 percent :rainbow_flag: ", "10 percent :rainbow_flag: ", "100 percent if you know it or not  YOU ARE GAE :rainbow_flag: ", "90 percent :rainbow_flag: ", "35 percent :rainbow_flag: ", "80 peercent :rainbow_flag: ", "85 percent :rainbow_flag: ", "6 percent :rainbow_flag: ", "GREAT YOU ARE NOT GAE!!! THANK GOD", "99 PERCENT :rainbow_flag: ", "idk"] //you can continue it like this , "another reply", "one more reply"]
-    message.channel.send(replies[Math.floor(Math.random() * replies.length)])
+  let replies = ['50 percent :rainbow_flag: ', '20 percent :rainbow_flag: ', '1 percent :rainbow_flag: ', '5 percent :rainbow_flag: ', '10 percent :rainbow_flag: ', '100 percent if you know it or not  YOU ARE GAE :rainbow_flag: ', '90 percent :rainbow_flag: ', '35 percent :rainbow_flag: ', '80 peercent :rainbow_flag: ', '85 percent :rainbow_flag: ', '6 percent :rainbow_flag: ', 'GREAT YOU ARE NOT GAE!!! THANK GOD', '99 PERCENT :rainbow_flag: ', 'idk'] //you can continue it like this , "another reply", "one more reply"]
+    message.reply(replies[Math.floor(Math.random() * replies.length)], { allowedMentions: { repliedUser: false } })
 };
 
 module.exports.config = {
