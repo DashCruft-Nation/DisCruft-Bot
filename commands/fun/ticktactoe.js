8f747e65b7f0daeada35cb3e590fe8ff46aa1582
John-Allan Lambert <asowner@outlook.com>
2021-03-24 20:05:55 +0000

    Eslint fix for tiktactoe.js

--- commands/fun/ticktactoe.js
+++ commands/fun/ticktactoe.js
@@ -1,15 +1,17 @@
-const Discord = require('discord.js')
-const reconlx = require("reconlx")
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-var */
+const Discord = require('discord.js');
+const reconlx = require('reconlx');
 
 
 module.exports.run = async (client, message, args) => {
-    var game = new reconlx.tictactoe({
-        message: message,
-       player_two: message.mentions.members.first(),
-        })
+	var game = new reconlx.tictactoe({
+		message: message,
+		player_two: message.mentions.members.first(),
+	});
 
 
-}
+};
 module.exports.config = {
 	name: 'ticktactoe',
 	aliases: [],
