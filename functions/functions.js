455376a15bdbf6fe1ec71215b27470896e7668e7
DashCruft <theporyafm12@gmail.com>
2021-03-24 12:05:11 -0700

    pp cmd fixed

--- functions/functions.js
+++ functions/functions.js
@@ -1,12 +1,3 @@
-/* eslint-disable no-unused-vars */
-function pepeSize() {
-	const random = Math.floor(Math.random() * 13) + 1;
-	const randomEqualSigns = '='.repeat(random);
-	const pepesize = `8${randomEqualSigns}>`;
-
-	return pepesize;
-}
-
 function clean(text) {
 	if (typeof (text) === 'string') {
 		return text.replace(/`/g, '`' + String.fromCharCode(8203)).replace(/@/g, '@' + String.fromCharCode(8203));
@@ -16,5 +7,4 @@ function clean(text) {
 
 module.exports = {
 	clean,
-	pepeSize
 };
