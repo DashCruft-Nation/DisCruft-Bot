a854bacf7c7940a3124c5cfa747420a1cd269167
Linker <66379820+Linker-123@users.noreply.github.com>
2021-03-25 21:47:00 +0300

    Faster unban command a better regex.

--- commands/mod/unban.js
+++ commands/mod/unban.js
@@ -10,16 +10,21 @@ module.exports.run = async (client, message, args) => {
 	if (!message.member.hasPermission('BAN_MEMBERS')) return message.channel.send('You are not allowed to unban members!');
 	if (!message.guild.me.hasPermission('BAN_MEMBERS')) return message.channel.send('I am not allowed to unban members!');
 	if (!args[0]) return message.reply('Please provide a user id to unban!', { allowedMentions: { repliedUser: false } });
-	const toUnBan = client.users.cache.get(args[0].match(/[1234567890]{18}/igm)[0]) || await client.users.fetch(args[0], true, true);
-	if (!toUnBan) return message.reply('You didnt provide a valid user!', { allowedMentions: { repliedUser: false } });
+	
+	const matchRegex = /[0-9]{16,19}/i;
+	if (!matchRegex.test(args[0])) {
+		return message.reply("Invalid user ID.", { allowedMentions: { repliedUser: false } });
+	}
+	
 	try {
-		(await message.guild.fetchBan(toUnBan));
+		await message.guild.members.unban(args[0]);
 	}
 	catch (err) {
-		return message.reply('The user isnt banned!', { allowedMentions: { repliedUser: false } });
+		return message.reply('Invalid user/isn\'t banned!', { allowedMentions: { repliedUser: false } });
 	}
-	message.guild.members.unban(toUnBan);
-	message.reply(`${toUnBan.username} is now unbanned!`, { allowedMentions: { repliedUser: false } });
+	
+	const unbanned = client.users.cache.get(args[0]);
+	message.reply(`${unbanned.username} is now unbanned!`, { allowedMentions: { repliedUser: false } });
 };
 module.exports.config = {
 	name: 'unban',
