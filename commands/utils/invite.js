2dc7de618d8191ef719a47441029c679f78435b9
DashCruft <theporyafm12@gmail.com>
2021-03-26 14:28:46 -0700

    xd

--- commands/utils/invite.js
+++ commands/utils/invite.js
@@ -8,7 +8,7 @@ module.exports.run = async (bot, message, args) => {
 		.setColor('#2f3136')
 		.setDescription(`${bot.user.username} is a community made \`open-source\` discord bot by **DashCruft Nation**! DisCruft is a multi-based bot where we are trying to include everything possible.
 
-    - You can invite DisCruft with this **[LINK](https://discord.com/api/oauth2/authorize?client_id=719267397017796689&permissions=1544420598&scope=bot)**!
+    - You can invite DisCruft with this **[LINK](https://discord.com/oauth2/authorize?client_id=823751227405893672&permissions=1544420598&scope=bot)**!
     - Check out our \`open-source\` github repository **[HERE](https://github.com/DashCruft-Nation/DisCruft-Bot)**!`)
 		.setThumbnail(bot.user.displayAvatarURL());
 	message.channel.send(embed);
