5079b101136e03c219dd27af1c305edaf0d3ac33
LegIt <77503777+LegItMate@users.noreply.github.com>
2021-03-28 12:08:31 +0530

    Added the mainColor to stats embed

--- commands/misc/stats.js
+++ commands/misc/stats.js
@@ -14,7 +14,7 @@ module.exports.run = async (client, message, args) => {
 			{ name: 'Client Info 🤖', value: `\`\`\`yaml\nName: ${client.user.tag}\nDisplayName: ${message.guild.me.displayName}\nID: ${client.user.id}\nServers: ${client.guilds.cache.size}\nCachedChannels: ${client.channels.cache.size}\nCachedUsers: ${client.users.cache.size}\nUptime: ${prettyMs(client.uptime)}\`\`\`` },
 			{ name: 'Vps Info 💻', value: `\`\`\`yaml\nPlatform: ${_os.platform()}\nCpuCores: ${_os.cpuCount()}\nCpuUsage: ${client.cpuusage}\nVpsRamUsage: ${Math.round(_os.totalmem() - _os.freemem())}/${Math.floor(_os.totalmem())} mb\`\`\`` },
 		)
-		.setColor('RANDOM')
+		.setColor(require('../../config.json').mainColor)
 		.setDescription('DisCruft is an open-source discord bot capable of many things such as Moderation, Music, Fun commands and etc!! It\'s being developed by a very actively contributing community! You can check it\'s source-code on it\'s [GitHub](https://github.com/DashCruft-Nation/DisCruft-Bot) 😉')
 		.setTitle('Bot Stats');
 	message.reply({ embed: embed, allowedMentions: { repliedUser: false } });
