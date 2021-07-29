1874b2821196ef121abdc3d554276733a2e82511
Christian Llupo <73932821+jebediah47@users.noreply.github.com>
2021-07-29 10:44:03 +0300

    Added ascii command!

---
+++ src/commands/misc/ascii.js
@@ -0,0 +1,38 @@
+/* eslint-disable */
+const figlet = require("figlet");
+const util = require("util");
+/**
+ * JSDOC
+ * @param {Discord.Client} client
+ * @param {Discord.Message} message
+ * @param {String[]} args
+ */
+
+const figletAsync = util.promisify(figlet);
+module.exports.run = async (client, message, args, Discord) => {
+  const text = args.join(" ");
+  if (!text) {
+    const embed1 = new Discord.MessageEmbed()
+      .setColor("RANDOM")
+      .setTitle("Notice!")
+      .setDescription("You need to add text to execute the command!")
+      .setTimestamp();
+    return message.channel.send({ embed: embed1 });
+  } else if (text.length > 20) {
+    const embed2 = new Discord.MessageEmbed()
+      .setColor("RANDOM")
+      .setTitle("Error!")
+      .setDescription("The inputted text has to be less than 20 characters!")
+      .setTimestamp();
+    return message.channel.send({ embed: embed2 });
+  }
+
+  const rendered = await figletAsync(text);
+  message.channel.send("```" + rendered + "```");
+};
+
+module.exports.config = {
+  name: "ascii",
+  aliases: ["unicode", "figlet"],
+  description: "This command turns inputted text to ascii art.",
+};
